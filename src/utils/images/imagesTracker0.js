12e5ae5f1fd4d1c7403ca180183b6ee642dead78
AngelinaBassano <bassanoangelina2002@gmail.com>
2024-06-20 12:53:15 +0200

    arreglo final de carpetas

---
+++ src/utils/images/imagesTracker0.js
@@ -0,0 +1,20 @@
+import fondoHome from "../../assets/generalHome/fondoHome.jpg"
+import logo from "../../assets/generalHome/logoConTextoBlanco.png"
+import mujerTos from "../../assets/generalHome/mujerTos.jpg"
+import niñoTos from "../../assets/generalHome/niñoTos.png"
+import niñoFiebre from "../../assets/generalHome/niñoFiebre.png"
+import pulmones from "../../assets/generalHome/pulmones.png"
+import sistemaHumano from "../../assets/generalHome/sistemaHumanoTos.png"
+
+const HomeImages ={
+  fondoHome,
+  logo,
+  mujerTos,
+  niñoFiebre,
+  niñoTos,
+  pulmones,
+  sistemaHumano
+};
+
+
+export default HomeImages;
