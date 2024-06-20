f90f844d360a54b28db121b6e82e734ffe48e83c
AngelinaBassano <bassanoangelina2002@gmail.com>
2024-06-20 13:29:16 +0200

    utils arreglo

--- src/utils/images/imagesHome.js
+++ src/utils/images/imagesHome.js
@@ -1,10 +1,20 @@
-import   from "../"
+import fondoHome from "../../assets/generalHome/fondoHome.jpg"
+import logo from "../../assets/generalHome/logoConTextoBlanco.png"
+import mujerTos from "../../assets/generalHome/mujerTos.jpg"
+import niñoTos from "../../assets/generalHome/niñoTos.png"
+import niñoFiebre from "../../assets/generalHome/niñoFiebre.png"
+import pulmones from "../../assets/generalHome/pulmones.png"
+import sistemaHumano from "../../assets/generalHome/sistemaHumanoTos.png"
 
-
-const lo que quieras poner ={
-  nombre de la imagen,
-   
+const HomeImages ={
+  fondoHome,
+  logo,
+  mujerTos,
+  niñoFiebre,
+  niñoTos,
+  pulmones,
+  sistemaHumano
 };
 
 
-export default lo que quieras poner;
+export default HomeImages;
