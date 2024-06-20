0921ed7010e27558f80d8862cd9aaeae9b551d01
tursdlc <ana89.sdlc@gmail.com>
2024-06-20 16:26:16 +0200

    primer cambio nav

--- src/components/navBarHome/NavBarHome.jsx
+++ src/components/navBarHome/NavBarHome.jsx
@@ -1,1 +1,23 @@
-HOLA
+function NavBarHome(){
+    return (
+        <>
+        <nav className="nav">
+        <div className="leftNavbar">
+            <h1 className="titleNavbar"><i className="iconNavbar"></i>COVIMAP</h1>
+        </div>
+        <div className="rightNavbar">
+            <ul>
+                <li>Home</li>
+                <li>Prevention</li>
+                <li>Quarantine</li>
+                <li>Pages</li>
+                <li>About</li>
+                <li>Help</li>
+                <button className="trackerNavbar">Tracker</button>
+            </ul>
+        </div>
+        </nav>
+        </>
+    )
+}
+export default NavBarHome
