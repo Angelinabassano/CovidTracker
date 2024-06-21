ea41972e143c545ddce840ba60b530e4f60dfe22
pilimuino <pili.muino@gmail.com>
2024-06-22 00:03:58 +0200

    incorporo código footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import Footer from './components/footerHome/FooterHome';
 
+const App = () => {
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
