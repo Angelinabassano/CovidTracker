59823a1acf132c4ed139ff3db935249bbdb8f8d0
Seda07 <gevorgyanseda0@gmail.com>
2024-06-28 13:02:16 +0200

    se sube unos cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,14 @@
 
+
+import List from './components/belowCardsTracker1/BelowCardsList.jsx';
+
+
+const App = () => {
+  return (
+    <div className="app">
+      <List />
+    </div>
+  );
+};
+
+export default App;
--- src/utils/images/imagesTracker1.js
+++ src/utils/images/imagesTracker1.js
@@ -1,10 +1,16 @@
-import   from "../"
+import telephoneT1 from "../../assets/otrosIconos/telephoneT1.png"
+import covidBlueDark from "../../assets/coronaVirus/covidBlueDark.png"
+import covidGreen from "../../assets/coronaVirus/covidGreen.png"
+import covidRed from "../../assets/coronaVirus/covidRed.png"
+import covidRedark from "../../assets/coronaVirus/covidRedark.png"
 
-
-const lo que quieras poner ={
-  nombre de la imagen,
-   
+const Tracer1Images = {
+  telephoneT1,
+  covidBlueDark,
+  covidGreen,
+  covidRed,
+  covidRedark,
 };
 
 
-export default lo que quieras poner;
+export default Tracer1Images;
