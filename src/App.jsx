0851e2b6192faef4dcb0241c68700fb496690474
Seda07 <gevorgyanseda0@gmail.com>
2024-06-28 16:03:45 +0200

    se sube cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from './components/footerHome/FooterHome';
 import SlideCardHome from "./components/slideCardsHome/SlideCardHome"
+
 function App() {   
   return (
     <div>
@@ -9,8 +10,22 @@ function App() {
          <SlideCardHome/>
          <Footer/>
     </div>
-  );
-};
-
+    </>
+  )
+}
 
 export default App;
+
+// import List from './components/belowCardsTracker1/BelowCardsList.jsx';
+
+
+// const App = () => {
+//   return (
+//     <div className="app">
+//       <List />
+//     </div>
+//   );
+// };
+
+// export default App;
+
