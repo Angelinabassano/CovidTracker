3ab60b58f4a460b0a42cb37015e88021bbc923ea
evymari <144184061+evymari@users.noreply.github.com>
2024-06-28 12:30:34 +0200

    upload changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
+import React from 'react'
+import './App.css'
+import Tracker0 from './layout/tracker0/Tracker0'
 
+function App() {   
+  return (
+    <>
+    <div>
+    <Tracker0></Tracker0>
+  
+    </div>
+    </>
+  )
+}
+
+export default App
