d8727eb787269a913fca905c5c6d994e331d7cca
tursdlc <ana89.sdlc@gmail.com>
2024-06-28 13:12:24 +0200

    animation reusableCard

--- src/components/reusableCardsTracker1/CardDesign.jsx
+++ src/components/reusableCardsTracker1/CardDesign.jsx
@@ -6,7 +6,7 @@ import "../../components/reusableCardsTracker1/reusableCards.css"
 
 const List = () => {
   const infoCard = [
-    { icon: imagesTracker1.covidBlueDark, label: 'Total Confirmed', value: '374111576', color: "#3639AE" },
+    { icon: imagesTracker1.covidBlueDark, label: 'Total Cases', value: '374111576', color: "#3639AE" },
     { icon: imagesTracker1.covidRed, label: 'Total Deaths', value: '4', color: '#ff0000' },
     { icon: imagesTracker1.covidGreen, label: 'Total Recovered', value: '3', color: '#82c519' },
     { icon: imagesTracker1.covidBlue, label: 'Total Active', value: '2', color: "#2c6dff"},
