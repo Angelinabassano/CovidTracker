4f7194290a3a0e33e258b553ebbdb56fa42d94b2
Seda07 <gevorgyanseda0@gmail.com>
2024-06-29 18:07:42 +0200

    se sube cambios de APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import './App.css'
-import NavBarHome from "./components/navBarHome/NavBarHome";
-import "./components/navBarHome/navBarHome.css"
-import BlueCardHome from './components/blueCardHome/BlueCardHome';
-import Footer from './components/footerHome/FooterHome';
-import SlideCardHome from "./components/slideCardsHome/SlideCardHome"
+// import React from 'react'
+// import './App.css'
+// import NavBarHome from "./components/navBarHome/NavBarHome";
+// import "./components/navBarHome/navBarHome.css"
+// import BlueCardHome from './components/blueCardHome/BlueCardHome';
+// import Footer from './components/footerHome/FooterHome';
+// import SlideCardHome from "./components/slideCardsHome/SlideCardHome"
 
-function App() {   
+// function App() {   
   
-  return (
-    <>
-    <div>
-         <BlueCardHome />
-         <NavBarHome/>
-         <SlideCardHome/>
-         <Footer/>
-    </div>
-    </>
-  )
-}
-
-export default App;
-
-// import List from './components/belowCardsTracker1/BelowCardsList.jsx';
-
-
-// const App = () => {
 //   return (
-//     <div className="app">
-//       <List />
-//     </div>
-//   );
-// };
+//     <>
+//      <div>
+//          <BlueCardHome />
+//          <NavBarHome/>
+//          <SlideCardHome/>
+//          <Footer/>
+//      </div>
+//     </>
+//   )
+// }
 
 // export default App;
+import React from 'react'
+import List from './components/belowCardsTracker1/BelowCardsList.jsx';
+
+
+const App = () => {
+  return (
+    <div className="app">
+      <List />
+    </div>
+  );
+};
+
+ export default App;
 
