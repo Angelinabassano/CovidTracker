20f716dd0d8216814adadb5c89a65fd4a907f97d
evymari <144184061+evymari@users.noreply.github.com>
2024-06-30 00:11:34 +0200

    none

---
+++ src/router/index.jsx
@@ -0,0 +1,40 @@
+import {  createBrowserRouter } from "react-router-dom";
+import { Tracker1 } from "../views/tracker 1/Tracker1.jsx";
+import Tracker2   from "../views/tracker2/Tracker2.jsx"
+
+import App from "../App.jsx";
+
+
+export const router = createBrowserRouter([
+       {
+      path: "/",
+       element: <App/>,
+        },
+         {
+            path: "/Tracker1",
+            element: <Tracker1></Tracker1>
+             
+         },
+         {
+            path: "/Tracker2",
+           element: <Tracker2></Tracker2>
+             ,
+         },
+         {
+            path: "/Tracker3",
+           element: <Tracker3></Tracker3>
+             ,
+         },
+         {
+            path: "/Tracker4",
+           element: <Tracker4></Tracker4>
+             ,
+         }
+       
+      
+     
+     
+   
+ ])
+
+
