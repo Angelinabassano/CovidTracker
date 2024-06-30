f177414e27bf943d92f84cf65ab300edc4d15627
Seda07 <gevorgyanseda0@gmail.com>
2024-06-30 19:33:15 +0200

    se sube ultimos cambios

--- src/App.jsx
+++ src/App.jsx
@@ -22,19 +22,3 @@ export default App
 
 
 
-// import React from 'react'
-// import Tracker1 from './views/tracker 1/Tracker1';
-
-// const App = () => {
-//   return (
-//     <>
-//       <div className="app">
-//         <Tracker1/>
-//       </div>
-//     </>  
-//   );
-// };
-
-// export default App;
-
-
