88a55e5b98f609206c95a984e16c485c18792088
Seda07 <gevorgyanseda0@gmail.com>
2024-06-30 19:33:15 +0200

    se sube ultimos cambios

--- src/App.jsx
+++ src/App.jsx
@@ -22,22 +22,6 @@ export default App
 
 
 
-// import React from 'react'
-// import Tracker1 from './views/tracker 1/Tracker1';
-
-// const App = () => {
-//   return (
-//     <>
-//       <div className="app">
-//         <Tracker1/>
-//       </div>
-//     </>  
-//   );
-// };
-
-// export default App;
-
-
 import List from "./components/reusableCardsTracker1/CardDesign.jsx"
 
 
