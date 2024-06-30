e7ff07b5e1b4e59a0ebeb79e9909e7ba1757167d
Seda07 <gevorgyanseda0@gmail.com>
2024-06-30 12:10:34 +0200

    se sube cambios de APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import Tracker1 from './views/tracker 1/Tracker1';
 
-const App = () => {
-  return (
-    <>
-      <div className="app">
-        <Tracker1/>
-      </div>
-    </>  
-  );
-};
 
-export default App;
+
+
+
+// import React from 'react'
+// import Tracker1 from './views/tracker 1/Tracker1';
+
+// const App = () => {
+//   return (
+//     <>
+//       <div className="app">
+//         <Tracker1/>
+//       </div>
+//     </>  
+//   );
+// };
+
+// export default App;
 
 
