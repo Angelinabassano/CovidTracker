e161fe6bf88bdd611026110854b01899f417f4bf
evymari <144184061+evymari@users.noreply.github.com>
2024-06-30 16:23:40 +0200

    none

--- src/components/trackerFourCountry/TrackerFourCountry.jsx
+++ src/components/trackerFourCountry/TrackerFourCountry.jsx
@@ -3,7 +3,7 @@ import '../trackerFourCountry/trackerFourContry.css'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 const TrackerFourCountry = () => {
-    
+  const [ TContry ,ContryData ] = useState([]);
        useEffect(()=>{
         
               axios.get('https://disease.sh/v3/covid-19/countries?sort=cases')
@@ -13,28 +13,33 @@ const TrackerFourCountry = () => {
       },[]);
   return (
     <>
+
+    <div className='WiseTabs'>
     <div className='CountryWiseTabs'>
-        <p>Covid-19 Country Wise - Tabs</p>
+        <p className='CountryWiseTabsTitle'>Covid-19 Country Wise - Tabs</p>
         <div>banderas</div>
          <div>cardsDos</div>
 
     </div>
-{/*     
+
+     
                   {TContry.map((element, i) =>{
                        return i <= 10 ?(
                         <div key={i}>
                            <div className="TFCountryContainer">
-                               <div className="InfoData">
+                               <div className="TFInfoData">
                                   <div className="TF-Flag"><img  src={element.countryInfo.flag}/></div>
                                   <div className="TF-Country"><p>{element.country}</p></div>
                               </div>
                                 <div className="TF-ConfirmedCase"><p>{element.cases}</p></div>
                            </div>
                          </div>
+                          
                         ): null
                   })
-                   } */}
+                                 } </div>  
     </>
+    
   )
 }
 
