4ef5284de8f121c75c35ba3c730f18671993c590
Seda07 <gevorgyanseda0@gmail.com>
2024-06-30 13:06:25 +0200

    se giarda los cambios de tracker1

--- src/views/pageOne/Tracker1.jsx
+++ src/views/pageOne/Tracker1.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import SideBar from '../../layout/sidebar/SideBar'
+import React from 'react';
+import SideBar from '../../layout/sidebar/SideBar';
+import List2 from '../../components/reusableCardsTracker1/CardDesign.jsx';
+import List from '../../components/belowCardsTracker1/BelowCardsList.jsx';
+import "../pageOne/tracker1.css"
+
 export const Tracker1 = () => {
   return (
     <>
-    
     <SideBar></SideBar>
+    <div className="tracker1">
+     <List2/>
+     <List/>
+    </div>
     </>
   )
 }
