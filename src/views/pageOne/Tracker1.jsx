e76f250451ef562da4826900f32737070bc5c9ec
Seda07 <gevorgyanseda0@gmail.com>
2024-06-30 18:33:24 +0200

    se sube areglo de tracker1

--- src/views/pageOne/Tracker1.jsx
+++ src/views/pageOne/Tracker1.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SideBar from '../../layout/sidebar/SideBar';
-import List2 from '../../components/reusableCardsTracker1/CardDesign.jsx';
+import ReusableCardsPage from '../../components/reusableCardsTracker1/CardDesign.jsx';
 import List from '../../components/belowCardsTracker1/BelowCardsList.jsx';
 import "../pageOne/tracker1.css"
 
@@ -9,7 +9,7 @@ export const Tracker1 = () => {
     <>
     <SideBar></SideBar>
     <div className="tracker1">
-     <List2/>
+     <ReusableCardsPage/>
      <List/>
     </div>
     </>
